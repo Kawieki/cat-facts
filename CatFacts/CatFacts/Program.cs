672697b69d7e7ba7d8b1a9a8fe1651ac407fda16
Łukasz Wiszniewski <lwiszniewski.code@gmail.com>
2025-07-17 12:35:03 +0200

    add Program.cs

---
+++ CatFacts/CatFacts/Program.cs
@@ -0,0 +1,31 @@
+﻿using CatFacts.Application;
+using CatFacts.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CatFacts;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var services = new ServiceCollection();
+        services.AddTransient<ICatFactService, CatFactService>();
+        services.AddTransient<ICatFileService, CatFileService>();
+        services.AddHttpClient<ICatFactService, CatFactService>();
+        
+        var serviceProvider = services.BuildServiceProvider();
+        
+        var catApp = new CatApp(
+            serviceProvider.GetRequiredService<ICatFactService>(),
+            serviceProvider.GetRequiredService<ICatFileService>()
+            );
+        try
+        {
+            catApp.RunAsync().GetAwaiter().GetResult();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while running the application: {ex.Message}");
+        } 
+    }
+}
